06ad504c28b193e2ea53175d4d26f07bb858cd44
Ben Hatsor <34835685+benhatsor@users.noreply.github.com>
2024-06-28 17:45:45 +0300

    Update Fetch.js

--- Fetch.js
+++ Fetch.js
@@ -1,2 +1,124 @@
 
-
+
+
+class Fetch {
+  
+  options = {
+    defaultResponseType: 'json'
+  };
+  
+  
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods
+  methods = ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS', 'TRACE', 'PATCH'];
+  
+  responseTypes = ['json', 'text', 'stream'];
+  
+  
+  constructor() {
+    
+    { defaultRespType } = this.options;
+    
+    this.methods.forEach(method => {
+      
+      // define methods for default resp type
+      this[method] =
+        this.getMethod(
+          method,
+          defaultRespType
+        );
+
+      // define methods for all resp types
+      this.responseTypes.forEach(respType => {
+        
+        this[method][respType] =
+          this.getMethod(
+            method,
+            respType
+          );
+        
+      });
+      
+    });
+    
+  }
+  
+  getMethod(method, respType) {
+    
+    return ((resource, options) => {
+      
+      options.method = method;
+      options.respType = respType;
+      
+      this.fetch(resource, options);
+      
+    });
+    
+  }
+  
+  
+  async function fetch(resource, options) {
+  
+    { isString, coerceValueToString } = this.util;
+    
+    
+    if (isString(resource)) {
+  
+      resource = coerceValueToString(resource);
+  
+      resource = options.urlPrefix + resource;
+      
+    }
+    
+    
+    const request = fetch(url, options);
+    
+    let resp = await request;
+    
+    
+    if (options.respType === 'json') {
+      
+      resp.body = await resp.json();
+      
+    } else if (options.respType === 'text') {
+      
+      resp.body = await resp.text();
+      
+    }
+    
+    
+    if (options.onlyBody) {
+      
+      return resp.body;
+      
+    }
+    
+    
+    return resp;
+    
+  }
+  
+  
+  util = {
+    
+    isString(x) {
+      return Object.prototype.toString.call(x) === '[object String]'
+    },
+    
+    // see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String#string_coercion
+    coerceValueToString(value) {
+      
+      return `${value}`;
+      
+    }
+    
+  }
+  
+}
+
+window.Fetch = new Fetch();
+
+
+
+
+  const api = 'https://api.letterboxd.com/api/v0/';
+  
