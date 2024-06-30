6847acb09d1e29dd55c18d16ad24927eb3d1abed
Ben Hatsor <34835685+benhatsor@users.noreply.github.com>
2024-06-30 13:13:57 +0300

    Update Fetch.js

--- Fetch.js
+++ Fetch.js
@@ -79,8 +79,19 @@ class Fetch {
     const request = fetch(resource, options);
     
     let resp = await request;
+    
+    
+    if (!resp.ok) {
       
-    resp = new Response(resp);
+      const descriptor = this.getStatusCodeDescriptor(resp.statusCode);
+      
+      if (descriptor) {
+      
+        console.error('Request failed: ' + descriptor);
+        
+      }
+      
+    }
     
     
     if (options.respType === 'json') {
@@ -134,6 +145,54 @@ class Fetch {
       return `${value}`;
       
     }
+    
+  }
+  
+  
+
+  // Status code descriptors
+  // Auto-generated from:
+  // https://httpwg.org/specs/rfc9110.html#overview.of.status.codes
+  
+  // Generation code:
+  /* const wrapper = document
+       .querySelector('[id="status.codes"]');
+     const classEl = wrapper
+       .querySelectorAll(':scope > section[id^="status"] > h3 > a[href^="#status"]');
+     const codeEl = wrapper
+       .querySelectorAll(':scope > section[id^="status"] > section[id^="status"] > h4 > a[href^="#status"]');
+  
+    let statusCodeClasses = [];
+    classEl.forEach(classTitle => {
+      let className = classTitle.textContent.split(' ');
+      className.pop();
+      statusCodeClasses.push(className.join(' '));
+    });
+    
+    let statusCodeDescriptors = {};
+    codeEl.forEach(codeTitle => {
+      const [code, ...descriptor] = codeTitle.textContent.split(' ');
+      if (descriptor.includes('(Unused)')) return;
+      statusCodeDescriptors[code] = descriptor.join(' ');
+    });
+  */
+  
+  statusCodeClasses = ['Informational', 'Successful', 'Redirection', 'Client Error', 'Server Error'];
+  
+  statusCodeDescriptors = {"100":"Continue","101":"Switching Protocols","200":"OK","201":"Created","202":"Accepted","203":"Non-Authoritative Information","204":"No Content","205":"Reset Content","206":"Partial Content","300":"Multiple Choices","301":"Moved Permanently","302":"Found","303":"See Other","304":"Not Modified","305":"Use Proxy","307":"Temporary Redirect","308":"Permanent Redirect","400":"Bad Request","401":"Unauthorized","402":"Payment Required","403":"Forbidden","404":"Not Found","405":"Method Not Allowed","406":"Not Acceptable","407":"Proxy Authentication Required","408":"Request Timeout","409":"Conflict","410":"Gone","411":"Length Required","412":"Precondition Failed","413":"Content Too Large","414":"URI Too Long","415":"Unsupported Media Type","416":"Range Not Satisfiable","417":"Expectation Failed","421":"Misdirected Request","422":"Unprocessable Content","426":"Upgrade Required","500":"Internal Server Error","501":"Not Implemented","502":"Bad Gateway","503":"Service Unavailable","504":"Gateway Timeout","505":"HTTP Version Not Supported"};
+  
+  
+  getStatusCodeDescriptor(code) {
+    
+    code = String(code);
+    const firstDigit = Number(code[0]);
+    
+    const codeClass = this.statusCodeClasses[firstDigit];
+    const codeDescriptor = this.statusCodeDescriptors[code];
+    
+    if (!codeClass || !codeDescriptor) return;
+    
+    return `${codeDescriptor} (${codeClass})`;
     
   }
   
